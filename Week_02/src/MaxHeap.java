009a0be4f50bcb6410aae4950928a4e2334958c5
xueyongxin <xueyongxin@guazi.com>
2020-09-03 19:46:11 +0800

    最大堆修改

--- Week_02/src/MaxHeap.java
+++ Week_02/src/MaxHeap.java
@@ -31,18 +31,31 @@ public class MaxHeap {
     }
 
 
+    private int parent(int idx) {
+        return (idx - 1) / 2;
+    }
+
+
+    private int leftChild(int idx) {
+        return idx * 2 + 1;
+    }
+
+
+    private int rightChild(int idx) {
+        return idx * 2 + 2;
+    }
+
+
     private void heapifyUp() {
 
-        // 与父节点比较，比父节点大则交换，否则return
         int current = size - 1;
         if (current == 0) return;
-        // 二叉堆 父节点为 i/2 (i为当前节点位置)
-        int parent = (current - 1) / 2;
+        int parent = parent(current);
         int tmp = heap[current];
         while (current > 0 && tmp > heap[parent]) {
             heap[current] = heap[parent];
             current = parent;
-            parent = (current - 1) / 2;
+            parent = parent(current);
         }
         heap[current] = tmp;
     }
@@ -61,14 +74,14 @@ public class MaxHeap {
     private void heapifyDown() {
 
         int parent = 0;
-        int left = parent * 2 + 1;
-        int right = parent * 2 + 2;
+        int left = leftChild(parent);
+        int right = rightChild(parent);
         while (left < size) {
             int replace = right < size && heap[right] >= heap[left] ? right : left;
             heap[parent] = heap[replace];
             parent = replace;
-            left = parent * 2 + 1;
-            right = parent * 2 + 2;
+            left = leftChild(parent);
+            right = rightChild(parent);
         }
 
     }
